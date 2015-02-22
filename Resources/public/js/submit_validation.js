f4b66461d11077e8bf4494e51e4c5422c100e402
alexandrzhulev <alexandrzhulev@gmail.com>
2015-02-22 02:03:26 +0200

    excluded appending empty error messages

--- Resources/public/js/submit_validation.js
+++ Resources/public/js/submit_validation.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.form_submit_button').on('click', function (e) {
         $.each($('*[data-constraint]'), function (key, val) {
             var errors = ($(val).attr('data-constraint')).split(' ');
-            if (errors.length > 1) {
+            if (errors.length >= 1) {
                 e.preventDefault();
                 e.stopPropagation();
                 $.each(errors, function (key, error) {
