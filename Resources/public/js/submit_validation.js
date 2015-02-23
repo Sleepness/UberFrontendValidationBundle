c3d43e0a6d5330eabb0b8c065c20f634b2e2647b
NovikovViktor <viktor.novikov95@gmail.com>
2015-02-23 09:25:33 +0200

    work on validation in JS

--- Resources/public/js/submit_validation.js
+++ Resources/public/js/submit_validation.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
     $('.form_submit_button').on('click', function (e) {
         $.each($('*[data-constraint]'), function (key, val) {
-            /* // this test code need to be removed after work done
-            if (errors.length > 1) {
             var errors = ($(val).attr('data-constraint')).split(' ');
-            if (errors.length >= 1) {
+            if (errors.length > 0) {
                 e.preventDefault();
                 e.stopPropagation();
                 $.each(errors, function (key, error) {
-                    $('#errors').append("<p>field " + $(val).attr('name') + " has error " + error + "!</p>");
+                    var name = 'Uber' + error + 'ValidationConstraint';
+                    var className = window[name];
+                    var constraintInstance = new className($(val), []);
+                    var errorMessage = constraintInstance.validate();
+                    $('#errors').append("<p>" + errorMessage + "</p>");
                 });
-            }*/
+            }
         });
     });
 });
