0d2fad64ad610d76325fac97251d6a293319f434
NovikovViktor <viktor.novikov95@gmail.com>
2015-02-23 12:17:39 +0200

    solve NotBlank undefined issue

--- Resources/public/js/constraints/NotBlank.js
+++ Resources/public/js/constraints/NotBlank.js
@@ -8,8 +8,9 @@ function UberNotBlankValidationConstraint(field, additional)
     this.message = 'Field {{value}} should not be blank';
 
     this.validate = function () {
+        var error = '';
         if (field.val().length === 0) {
-           var error = this.message.replace('{{value}}', String(field.attr('name')));
+            error = this.message.replace('{{value}}', String(field.attr('name')));
         }
 
         return error;
