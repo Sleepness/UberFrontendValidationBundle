c26651a9ade5ca30063f339beadf7ec0347fce44
alexandrzhulev <alexandrzhulev@gmail.com>
2015-03-03 00:29:16 +0200

    added validation for Blank constraint

---
+++ Resources/public/js/constraints/Blank.js
@@ -0,0 +1,21 @@
+/**
+ * Check if field is empty
+ *
+ * @author Alexandr Zhulev <alexandrzhulev@gmail.com>
+ * @constructor
+ */
+function UberBlankValidationConstraint(field) {
+    this.message = 'Field {{value}} should be blank';
+
+    this.validate = function () {
+        var error = '';
+        if (field.val().length > 0) {
+            error = this.message.replace('{{value}}', String(parse_field_name(field.attr('name'))));
+            if (field.attr('data-message') != '') {
+                error = field.attr('data-message');
+            }
+        }
+
+        return error;
+    }
+}
