62408f835025f308578d41c24d9f9549f53827ad
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-02 09:03:53 +0200

    work on EqualTo validator constraint

--- Resources/public/js/constraints/EqualTo.js
+++ Resources/public/js/constraints/EqualTo.js
@@ -5,9 +5,15 @@
  * @constructor
  */
 function UberEqualToValidationConstraint(field) {
-    this.message = 'This {{value}} cannot be null';
+    this.message = 'This {{value}} must be equal to {{ value }}';
 
     this.validate = function () {
+        var error = '';
+        if (field.val() != field.attr('data-value') || field.val() == '') {
+            var message = field.attr('data-message');
+            error = message.replace('{{ compared_value }}', field.attr('data-value'));
+        }
 
+        return error;
     }
 }
