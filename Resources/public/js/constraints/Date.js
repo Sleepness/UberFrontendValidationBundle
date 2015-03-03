fb5f40a82599ea1bbf47c2e3c5d0243a9770570a
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-03 12:28:14 +0200

    add Date constraint validator

---
+++ Resources/public/js/constraints/Date.js
@@ -0,0 +1,22 @@
+/**
+ * Check if element value is valid Date value
+ *
+ * @author Viktor Novikov <viktor.novikov95@gmail.com>
+ * @constructor
+ */
+function UberDateValidationConstraint(field) {
+    this.message = 'This {{value}} is not valid date';
+
+    this.validate = function () {
+        var pattern = /^(\d{4})-(\d{2})-(\d{2})$/;
+        var error = '';
+        if (!pattern.test(field.val())) {
+            error = this.message.replace('{{value}}', String(parse_field_name(field.attr('name'))));
+            if (field.attr('data-message') != '') {
+                error = field.attr('data-message');
+            }
+        }
+
+        return error;
+    }
+}
