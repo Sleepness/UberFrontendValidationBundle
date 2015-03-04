510d391ad4ad85fc6cbb0fff7457199afa0f6982
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-04 22:11:57 +0200

    removed useless line from submit handler

--- Resources/public/js/submit_validation.js
+++ Resources/public/js/submit_validation.js
@@ -6,7 +6,6 @@
 (function ($) {
     $(document).ready(function () {
         $('.form_submit_button').on('click', function (e) {
-            $('#errors').html("");
             $.each($('*[data-constraint]'), function (key, val) {
                 var errors = ($(val).attr('data-constraint')).split(' ');
                 if (errors.length > 0) { // for now we cant submit form even if it valid, I'll fix that in future
