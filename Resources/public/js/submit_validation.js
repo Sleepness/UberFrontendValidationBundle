b38ec6dfda6423b60a00770f3d666de46cbb2d6f
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-05 12:04:44 +0200

    removed useless lines, solve multiple forms on single page issue

--- Resources/public/js/submit_validation.js
+++ Resources/public/js/submit_validation.js
@@ -4,27 +4,26 @@
  * @author Viktor Novikov <viktor.novikov95@gmail.com>
  */
 (function ($) {
-    $(document).ready(function () {
-        $('.form_submit_button').on('click', function (e) {
-            $.each($('*[data-constraint]'), function (key, val) {
-                var errors = ($(val).attr('data-constraint')).split(' ');
-                if (errors.length > 0) { // for now we cant submit form even if it valid, I'll fix that in future
-                    $.each(errors, function (key, error) {
-                        var name = 'Uber' + error + 'ValidationConstraint';
-                        var className = window[name];
-                        if (typeof(className) == "function") {
-                            var constraintInstance = new className($(val), []);
-                            var errorMessage = constraintInstance.validate();
-                            if (!errorMessage == '') {
-                                e.preventDefault();
-                                e.stopPropagation();
-                                $(val).addClass('invalid-field');
-                                $(val).parent('div').append("<p class='errors'>" + errorMessage + "</p>");
-                            }
+    $('.form_submit_button').click(function (e) {
+        var $form = $(this).parent('form');
+        $.each($form.find('*[data-constraint]'), function (key, val) {
+            var errors = ($(val).attr('data-constraint')).split(' ');
+            if (errors.length > 0) { // for now we cant submit form even if it valid, I'll fix that in future
+                $.each(errors, function (key, error) {
+                    var name = 'Uber' + error + 'ValidationConstraint';
+                    var className = window[name];
+                    if (typeof(className) == "function") {
+                        var constraintInstance = new className($(val), []);
+                        var errorMessage = constraintInstance.validate();
+                        if (!errorMessage == '') {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            $(val).addClass('invalid-field');
+                            $(val).parent('div').append("<p class='errors'>" + errorMessage + "</p>");
                         }
-                    });
-                }
-            });
+                    }
+                });
+            }
         });
     });
 })(jQuery);
