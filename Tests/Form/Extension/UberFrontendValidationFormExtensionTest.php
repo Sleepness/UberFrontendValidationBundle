2f6d1d6607733f9d619ab44ee55637b9c51c8017
alexandrzhulev <alexandrzhulev@gmail.com>
2015-03-31 00:52:58 +0300

    fixed running tests

--- Tests/Form/Extension/UberFrontendValidationFormExtensionTest.php
+++ Tests/Form/Extension/UberFrontendValidationFormExtensionTest.php
@@ -37,14 +37,10 @@ class UberFrontendValidationFormExtensionTest extends WebTestCase
     public function testPrepareConstraintsAttributes()
     {
         $entityMetadata = $this->validator->getMetadataFor('Sleepness\UberFrontendValidationBundle\Tests\Fixtures\Entity\Post');
-        var_dump($entityMetadata);die;
-        $preparedConstraintsAttributes = $this->extension->prepareConstraintsAttributes('post[title]', $entityMetadata, array('Default'));
-//        $reflectedMethod = new ReflectionMethod($this->extension, 'prepareConstraintsAttributes');
-//        $reflectedMethod->setAccessible(TRUE);
-//        $preparedConstraintsAttributes = $reflectedMethod->invoke($this->extension, 'post[title]', $entityMetadata, array('Default'));
+        $reflectedMethod = new ReflectionMethod($this->extension, 'prepareConstraintsAttributes');
+        $reflectedMethod->setAccessible(TRUE);
+        $preparedConstraintsAttributes = $reflectedMethod->invoke($this->extension, 'post[title]', $entityMetadata, array('Default'));
         $fullFieldNames = array_keys($preparedConstraintsAttributes);
-        var_dump($preparedConstraintsAttributes);die;
-//        var_dump($preparedConstraintsAttributes[$fullFieldNames[0]]);die;
         $constraintNames = array_keys($preparedConstraintsAttributes[$fullFieldNames[0]]);
         $constraintProperties = array_keys($preparedConstraintsAttributes[$fullFieldNames[0]][$constraintNames[0]]);
         $this->assertEquals('post[title]', $fullFieldNames[0]);
