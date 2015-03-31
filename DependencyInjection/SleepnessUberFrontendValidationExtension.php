e54fedc60b7464111b5911ffb5be1f608b4774f2
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-31 12:39:07 +0300

    delete useless configuration of bundle

--- DependencyInjection/SleepnessUberFrontendValidationExtension.php
+++ DependencyInjection/SleepnessUberFrontendValidationExtension.php
@@ -19,8 +19,6 @@ class SleepnessUberFrontendValidationExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
